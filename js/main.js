30c20c09b8945d4e9a2bb930c9d15c067cd9236e
Luca <mylince@gmail.com>
2020-02-19 17:09:26 +0100

    Add

---
+++ js/main.js
@@ -0,0 +1,48 @@
+//ES DADI:
+//Se scrivi "lancia i dadi", uscirà un numero da 1 a 10; se è incluso tra 1 e 5 perderai, se invece è incluso tra 5 e 10, vincerai.
+ var lanciaDadi = parseInt(prompt('Scrivi "lancia" per lanciare i dadi e scoprire se hai vinto 🤹🏻 \n \r ----> Se usciraù un numero maggiore di 5 avrai vinto 👍🏻 \n \r ----> Se uscirà un numero mino di 5, avrai perso 👎🏻'));
+ var lanciaDadi = Math.round(Math.random() * 10);
+console.log(lanciaDadi);
+if (!isNaN(lanciaDadi)) {
+
+
+    if ( (lanciaDadi > 0) && ( lanciaDadi <= 5)){
+        document.getElementById('haiperso').innerHTML = 'Hai perso, è uscito: ' + lanciaDadi + '! 😞';
+    } else if((lanciaDadi > 5) && (lanciaDadi <= 10)){
+        document.getElementById('haivinto').innerHTML = 'Hai vinto, è uscito: ' + lanciaDadi + '! 😃';
+    }
+
+
+}else {
+    console.log('hai sbagliato');
+}
+
+
+//ES EMAIL:
+//Chiedi la mail con un prompt
+//Controlla che sia nella LISTA
+//Stampa un messaggio
+
+var chiediMail = prompt('Inserisci qui la tua mail');
+
+var listaMail = ['tony@stark.us', 'hulk@banner.us', 'steve@rogers.us', 'thor@thunder.us', 'vedova@nera.us']
+var ironman = 'tony@stark.us';
+var mailCorretta = false;
+
+for (var i = 0; i < listaMail.length; i++) {
+     var mailCorrette = listaMail[i];
+     if (mailCorrette == chiediMail){
+        mailCorretta = true;
+    }
+ }
+
+
+ if (mailCorretta == true){
+    console.log('Mail accettata, puoi accedere!');
+    document.getElementById('tonystark').innerHTML = ironman;
+
+}
+else {
+    console.log('Mail declinata, non puoi accedere');
+    document.getElementById('')
+}
